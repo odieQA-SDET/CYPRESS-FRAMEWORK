e9b6bf1e3479e005be909689ea42d9320cbfd28f
QodieQA-SDET <ahmaddodi.mr@gmail.com>
2023-04-02 15:10:05 +0700

    Cypress Simple Factorial - initial commit

---
+++ cypress/e2e/Simple_Factorial-spec.cy.js
@@ -0,0 +1,44 @@
+describe('The factorial Verify', () => {
+  it('The factorial of 0 is equal to 1', () => {
+    cy.visit('http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/')
+    cy.get('#number').type('0')
+    cy.get('#getFactorial').click()
+    cy.get('div.row').should('contain','The factorial of 0 is: 1')
+  })
+    it('Factorial of positive integer should be calculated correctly', () => {
+    cy.visit('http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/')
+    cy.get('#number').type('5')
+    cy.get('#getFactorial').click()
+    cy.get('div.row').should('contain','The factorial of 5 is: 120')
+  })
+  it('Negative integer input should display error message', () => {
+    cy.visit('http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/')
+    cy.get('#number').type('-5')
+    cy.get('#getFactorial').click()
+    cy.get('div.row').should('contain','')
+  })
+  it('Non-numeric input should display error message', () => {
+    cy.visit('http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/')
+    cy.get('#number').type('test')
+    cy.get('#getFactorial').click()
+    cy.get('div.row').should('contain','Please enter an integer')
+  })
+  it('Decimal number input should display error message', () => {
+    cy.visit('http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/')
+    cy.get('#number').type('5.5')
+    cy.get('#getFactorial').click()
+    cy.get('div.row').should('contain','Please enter an integer')
+  })
+  it('Large number input should be calculated correctly', () => {
+    cy.visit('http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/')
+    cy.get('#number').type('170')
+    cy.get('#getFactorial').click()
+    cy.get('div.row').should('contain','The factorial of 170 is: 7.257415615307994e+306')
+  })
+  it('Large number input should be calculated correctly with null output', () => {
+    cy.visit('http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/')
+    cy.get('#number').type('171')
+    cy.get('#getFactorial').click()
+    cy.get('div.row').should('contain','The factorial of 171 is: null')
+  })
+})
