67538e32f6ed73645a608c4a49533a6ca516cb08
QodieQA-SDET <ahmaddodi.mr@gmail.com>
2023-04-05 16:48:16 +0700

    update-git action testing - initial commit

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl:"http://z29vzcbmaw5kaw5nigzvcib5b3u.prospace.io/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
